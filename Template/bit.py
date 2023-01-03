c7b65aa7882ab7203046ac07779c5a3619a9c642
MikeAlwaysCode <chaoszhang@outlook.com>
2023-01-03 15:33:48 +0800

    增加了并查集和树状数组模板

---
+++ Template/bit.py
@@ -0,0 +1,89 @@
+# 模板一：BIT
+class BIT:
+    def __init__(self, n: int):
+        self.nums = [0] * (n + 1)
+        self.n = n
+        self.BITree = [0] * (self.n + 1)
+        
+    def lowbit(self, x: int) -> int:
+        return x & -x
+    
+    def query(self, x: int) -> int:
+        ans = 0
+        while x:
+            ans += self.BITree[x]
+            x -= self.lowbit(x)
+        return ans
+
+    def add(self, x: int, val: int):
+        while x <= self.n:
+            self.BITree[x] += val
+            x += self.lowbit(x)
+
+    def update(self, x: int, val: int) -> None:
+        self.add(x + 1, val - self.nums[x])
+        self.nums[x] = val
+
+
+    # 逆序对
+    def getPairOfInversion(nums: list) -> int:
+        bit = BIT(max(nums))
+        res = 0
+        for i in range(len(nums) - 1, -1, -1):
+            res += bit.query(nums[i])
+            bit.update(nums[i], 1)
+        return res
+
+# 模板二
+class NumArray:
+    def __init__(self, nums: list[int]):
+        self.nums = nums
+        self.tree = [0] * (len(nums) + 1)
+        for i, num in enumerate(nums, 1):
+            self.add(i, num)
+    def add(self, index: int, val: int):
+        while index < len(self.tree):
+            self.tree[index] += val
+            index += index & -index
+    def prefixSum(self, index) -> int:
+        s = 0
+        while index:
+            s += self.tree[index]
+            index &= index - 1
+        return s
+    def update(self, index: int, val: int) -> None:
+        self.add(index + 1, val - self.nums[index])
+        self.nums[index] = val
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefixSum(right + 1) - self.prefixSum(left)
+
+
+# 模板三
+arr = []
+n = 10
+ans = 0
+
+discretization = {v:i for i, v in enumerate(sorted(set(arr)))}
+tn = len(discretization)
+BITree = [0] * (tn + 1)
+
+def lowbit(x: int) -> int:
+    return x & -x
+
+def query(x: int) -> int:
+    ans = 0
+    while x:
+        ans += BITree[x]
+        x -= lowbit(x)
+    return ans
+
+def add(x: int, val: int):
+    while x <= tn:
+        BITree[x] += val
+        x += lowbit(x)
+
+for i in range(n - 1, -1, -1):
+    idx = discretization[arr[i]]
+    ans += query(idx+1)
+    add(idx+1, 1)
+
---
+++ Template/dsu.py
@@ -0,0 +1,71 @@
+# 模板一：封装成类
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.n = n
+        # 当前连通分量数目
+        self.setCount = n
+    
+    def findset(self, x: int) -> int:
+        # if self.parent[x] == x:
+        #     return x
+        # self.parent[x] = self.findset(self.parent[x])
+        # return self.parent[x]
+        cur = x
+        while x != self.parent[x]:
+            x = self.parent[x]
+        if cur != x:
+            self.parent[cur] = x
+        return x
+    
+    def unite(self, x: int, y: int) -> bool:
+        x, y = self.findset(x), self.findset(y)
+        if x == y:
+            return False
+        if self.size[x] < self.size[y]:
+            x, y = y, x
+        self.parent[y] = x
+        self.size[x] += self.size[y]
+        self.setCount -= 1
+        return True
+    
+    def connected(self, x: int, y: int) -> bool:
+        x, y = self.findset(x), self.findset(y)
+        return x == y
+
+
+# 模板二：
+n = 10
+fa = list(range(n + 1))
+s = [0] * (n + 1)
+
+def find(x: int):
+    cur = x
+    while x != fa[x]:
+        x = fa[x]
+    if cur != x:
+        fa[cur] = x
+    return x
+
+def union(fr: int, to: int):
+    fa[find(fr)] = find(to)
+
+# ans = [0] * n
+# for i in range(n - 1, 0, -1):
+#     x = removeQueries[i]
+#     fa[x] = find(x + 1)
+#     to = fa[x] 
+#     s[to] += s[x] + nums[x]
+#     ans[i-1] = max(ans[i], s[to])
+
+# 模板三：
+parent = list(range(n + 1))
+
+def find(index: int) -> int:
+    if parent[index] != index:
+        parent[index] = find(parent[index])
+    return parent[index]
+
+def union(index1: int, index2: int):
+    parent[find(index1)] = find(index2)
