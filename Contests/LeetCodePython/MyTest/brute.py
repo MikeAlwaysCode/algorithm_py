4b2bf8367ddf8f984a893d3217e29eacac1a698b
MikeAlwaysCode <chaoszhang@outlook.com>
2023-01-03 17:00:43 +0800

    remove leetcode unsed files

--- Contests/LeetCodePython/MyTest/brute.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-
-for _ in range(t):
-    # n = int(input())
-    # n, l, r = map(int, input().split())
-    # a = list(map(int, input().split()))
-
-    
-    # print(ans)
--- Contests/LeetCodePython/MyTest/gen.py
+++
@@ -1,6 +0,0 @@
-import sys, random
-
-random.seed(int(sys.argv[1]))
-
-# generate test cases with same format as given in problem
-
