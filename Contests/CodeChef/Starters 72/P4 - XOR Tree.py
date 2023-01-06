f35c7809fcdb1e4871bd3aba03351167e59f1be5
MikeAlwaysCode <chaoszhang@outlook.com>
2023-01-06 17:53:27 +0800

    update 01trie

--- Contests/CodeChef/Starters 72/P4 - XOR Tree.py
+++ Contests/CodeChef/Starters 72/P4 - XOR Tree.py
@@ -11,42 +11,72 @@ ints = lambda: list(map(int, input().split()))
 # MOD = 998244353
 MOD = 10 ** 9 + 7
 
-HIGH_BIT = 30
+class BinaryTrie:
+    def __init__(self, max_bit: int = 30):
+        self.inf = 1 << 63
+        self.to = [[-1], [-1]]
+        self.cnt = [0]
+        self.max_bit = max_bit
 
-class TrieNode:
-    def __init__(self):
-        self.children = [None, None]
-        self.sum = 0
-        
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def add(self, num: int, diff: int) -> None:
-        cur = self.root
-        for k in range(HIGH_BIT, -1, -1):
+    def add(self, num: int) -> None:
+        cur = 0
+        self.cnt[cur] += 1
+        for k in range(self.max_bit, -1, -1):
             bit = (num >> k) & 1
-            if not cur.children[bit]:
-                cur.children[bit] = TrieNode()
-            cur = cur.children[bit]
-            cur.sum += diff
+            if self.to[bit][cur] == -1:
+                self.to[bit][cur] = len(self.cnt)
+                self.to[0].append(-1)
+                self.to[1].append(-1)
+                self.cnt.append(0)
+            cur = self.to[bit][cur]
+            self.cnt[cur] += 1
 
-    def get(self, num: int, x: int) -> int:
-        res = 0
-        cur = self.root
-        for k in range(HIGH_BIT, -1, -1):
+    def remove(self, num: int) -> bool:
+        if self.cnt[0] == 0: return False
+        cur = 0
+        rm = [0]
+        for k in range(self.max_bit, -1, -1):
             bit = (num >> k) & 1
-            if (x >> k) & 1:
-                if cur.children[bit]:
-                    res += cur.children[bit].sum
-                if not cur.children[bit ^ 1]:
-                    return res
-                cur = cur.children[bit ^ 1]
+            cur = self.to[bit][cur]
+            if cur == -1 or self.cnt[cur] == 0: return False
+            rm.append(cur)
+        for cur in rm: self.cnt[cur] -= 1
+        return True
+
+    def count(self, num: int):
+        cur = 0
+        for k in range(self.max_bit, -1, -1):
+            bit = (num >> k) & 1
+            cur = self.to[bit][cur]
+            if cur == -1 or self.cnt[cur] == 0: return 0
+        return self.cnt[cur]
+
+    # Get max result for constant x ^ element in array
+    def max_xor(self, x: int) -> int:
+        if self.cnt[0] == 0: return -self.inf
+        res = cur = 0
+        for k in range(self.max_bit, -1, -1):
+            bit = (x >> k) & 1
+            nxt = self.to[bit ^ 1][cur]
+            if nxt == -1 or self.cnt[nxt] == 0:
+                cur = self.to[bit][cur]
             else:
-                if not cur.children[bit]:
-                    return res
-                cur = cur.children[bit]
-        res += cur.sum
+                cur = nxt
+                res |= 1 << k
+        return res
+
+    # Get min result for constant x ^ element in array
+    def min_xor(self, x: int) -> int:
+        if self.cnt[0] == 0: return self.inf
+        res = cur = 0
+        for k in range(self.max_bit, -1, -1):
+            bit = (x >> k) & 1
+            nxt = self.to[bit][cur]
+            if nxt == -1 or self.cnt[nxt] == 0:
+                res |= 1 << k
+                cur = self.to[bit ^ 1][cur]
+            else:
+                cur = nxt
         return res
 
 def solve() -> None:
@@ -59,10 +89,23 @@ def solve() -> None:
         tree[u].append(v)
         tree[v].append(u)
 
+    bt = BinaryTrie()
     ans = 0
     def dfs(x, p, xor, m):
         nonlocal ans
-
+        bt.add(vals[x])
+        xor ^= vals[x]
+        c = len(tree[x])
+        if x != 1: c -= 1
+        if c == 0:  # is leaf
+            ans += bt.max_xor(xor) * pow(m, MOD - 2, MOD) % MOD
+        else:
+            m = m * c % MOD
+            for u in tree[x]:
+                if u == p: continue
+                dfs(u, x, xor, m)
+        bt.remove(vals[x])
+    dfs(1, 0, 0, 1)
     '''
     parent = [0] * (n + 1)
 
@@ -84,7 +127,6 @@ def solve() -> None:
             ans += cal(xor, x, m)
             continue
         m *= c
-        # m = m * pow(c, MOD - 2, MOD) % MOD
         for u in tree[x]:
             if u == p: continue
             parent[u] = x
@@ -92,6 +134,7 @@ def solve() -> None:
     '''
     print(ans % MOD)
 
+sys.setrecursionlimit(500005)
 t = int(input())
 for _ in range(t):
     solve()
