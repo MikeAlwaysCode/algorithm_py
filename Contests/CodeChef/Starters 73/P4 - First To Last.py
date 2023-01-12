1cc6c88b88e6c2eb959c1a2e907e2656f6508002
MikeAlwaysCode <chaoszhang@outlook.com>
2023-01-12 17:52:26 +0800

    update CodeChef Starters 73

--- Contests/CodeChef/Starters 73/P4 - First To Last.py
+++ Contests/CodeChef/Starters 73/P4 - First To Last.py
@@ -11,30 +11,50 @@ ints = lambda: list(map(int, input().split()))
 # MOD = 998244353
 # MOD = 10 ** 9 + 7
 
+def lis(nums, cmp = lambda x, y: x < y):
+    P = [0] * len(nums)
+    M = [0] * (len(nums) + 1)
+    L = 0
+
+    for i in range(len(nums)):
+        lo, hi = 1, L
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if cmp(nums[M[mid]], nums[i]):
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        newL = lo
+        P[i] = M[newL - 1]
+        M[newL] = i
+
+        L = max(L, newL)
+
+    S = [0] * L
+    k = M[L]
+
+    for i in range(L - 1, -1, -1):
+        S[i], k = nums[k], P[k]
+
+    return S
+
 def solve() -> None:
     n, m, k = map(int, input().split())
     spec = []
     for _ in range(k):
-        spec.append(ints())
+        x, y = map(int, input().split())
+        if x < n and y < m:
+            spec.append([x, y])
+    
+    ans = n + m - 2
+    if spec:
+        spec.sort(key=lambda x: (x[0], -x[1]))
+        x, y = map(list, zip(*spec))
+        ans -= len(lis(y))
 
-    if k == 0:
-        print(n + m - 2)
-        return
-
-    rows, cols = map(set, zip(*spec))
-    dr = {r:i for i, r in enumerate(sorted(rows))}
-    dc = {c:i for i, c in enumerate(sorted(cols))}
-    spec.sort()
-    dp = [[1] * len(cols) for _ in range(len(rows))]
-    for i in range(k):
-        di = dr[spec[i][0]]
-        dj = dc[spec[i][1]]
-        if di > 0:
-            dp[di][dj] = max(dp[di][dj], dp[di - 1][dj])
-        if dj > 0:
-            dp[di][dj] = max(dp[di][dj], dp[di][dj - 1])
-
-    print(n + m - 2 - dp[len(rows) - 1][len(cols) - 1])
+    print(ans)
 
 t = int(input())
 for _ in range(t):
