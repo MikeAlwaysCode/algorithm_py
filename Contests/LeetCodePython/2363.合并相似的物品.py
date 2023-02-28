ebb5bd81b1aa8556e7f3ae685c261a9f3600b351
MikeAlwaysCode <chaoszhang@outlook.com>
2023-02-28 22:28:29 +0800

    Codeforces Round 854

--- Contests/LeetCodePython/2363.合并相似的物品.py
+++ Contests/LeetCodePython/2363.合并相似的物品.py
@@ -76,9 +76,13 @@
 # 
 # 
 #
+from collections import Counter
+from typing import List
+
 
 # @lc code=start
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+        return sorted((Counter(dict(items1)) + Counter(dict(items2))).items())
 # @lc code=end
 
